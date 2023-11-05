da415af5925edf65f8723eb6fdf31afc1827aee6
vikkuzz <simpat007@yandex.ru>
2023-11-06 00:37:13 +0300

    test

--- pages/api/socials.js
+++ pages/api/socials.js
@@ -2,6 +2,7 @@ import { socials } from './data/socials';
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
+    console.log(socials)
     res.status(200).json(socials);
   };
 }
