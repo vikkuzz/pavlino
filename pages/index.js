e8db22c9ff65d249744dbdc96569d5fd42a47efa
vikkuzz <simpat007@yandex.ru>
2023-11-05 20:36:15 +0300

    test env

--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,32 @@ import { useEffect } from "react";
 import Socials from "../components/Socials";
 import styles from "../styles/Home.module.scss";
 
-// export const getStaticProps = async () => {
-//   const response = await fetch(
-//     `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/socials`
-//   );
+export const getStaticProps = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/socials`,
+    { mode: "no-cors",}
+  );
 
   
-//   const data = await response.json();
+  const data = await response.json();
 
-//   if (!data) {
-//     return {
-//       notFound: true,
-//     };
-//   }
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
 
-//   return {
-//     props: { socials: data },
-//   };
-// };
+  return {
+    props: { socials: data },
+  };
+};
 
 const Home = ({ socials }) => {
   useEffect(()=>{
     async function getSocials(){
       const response = await fetch(
-      `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/socials`
+      `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/socials`,
+     { mode: "no-cors",}
     );
     console.log(response.json())    
     }
@@ -35,7 +37,7 @@ const Home = ({ socials }) => {
   },[])
   return (
     <div className={styles.wrapper}>
-      {/* <Socials socials={socials} /> */}
+      <Socials socials={socials} />
     </div>
   );
 };
