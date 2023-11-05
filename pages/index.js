bf78f9dada0696935fcbc606defc19a0b0b3052b
vikkuzz <simpat007@yandex.ru>
2023-11-05 19:04:57 +0300

    test env

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,38 @@ import Socials from "../components/Socials";
 import styles from "../styles/Home.module.scss";
 
 export const getStaticProps = async () => {
-  const response = await fetch(
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/socials`
-  );
-  const data = await response.json();
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
+  // const response = await fetch(
+  //   `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/socials`
+  // );
+  const socials = [
+    {
+      id: 1,
+      icon: 'youtube',
+      path: 'https://youtube.com/YauhenKavalchuk',
+    },
+    {
+      id: 2,
+      icon: 'instagram',
+      path: 'https://instagram.com/YauhenKavalchuk',
+    },
+    {
+      id: 3,
+      icon: 'linkedin',
+      path: 'https://linkedin.com/in/YauhenKavalchuk',
+    },
+    {
+      id: 4,
+      icon: 'vk',
+      path: 'https://vk.com/YauhenKavalchuk',
+    },
+    {
+      id: 5,
+      icon: 'twitter',
+      path: 'https://twitter.com/YauhenKavalchuk',
+    },
+  ]
+  const data = await socials.json();
 
   if (!data) {
     return {
@@ -21,7 +49,7 @@ export const getStaticProps = async () => {
 const Home = ({ socials }) => {
   return (
     <div className={styles.wrapper}>
-      <Socials socials={socials} />
+      {/* <Socials socials={socials} /> */}
     </div>
   );
 };
