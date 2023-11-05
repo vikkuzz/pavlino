e82150673fa9a957d7de0238b41fefa78ca41e2b
vikkuzz <simpat007@yandex.ru>
2023-11-05 21:48:10 +0300

    test env

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
  
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap"
